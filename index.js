da32af6cd5b97c53b8c566c263085c0b2bc9b378
Bsilvs95 <brian9550@gmail.com>
2019-11-25 17:30:14 -0500

    sequelize Homework

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
 const express = require('express')
-const teams = require('./teams.json')
 const bodyParser = require('body-parser')
+const models = require('./models')
+const { Op } = require('sequelize')
 
 const app = express()
 
 
 
-app.get('/teams', (request, response) => {
-
+app.get('/teams', async (request, response) => {
+    const teams = await models.Teams.findAll()
     response.send(teams)
 
 })
 
+app.get('/teams/:id', async (request, response) => {
 
-//why do i not need this, is it cause above
-/* app.get('/teams/', (request, response) => {
+    /*  const matchingTeams = teams.filter((team) => {
+          return team.id === parseInt(request.params.id) || team.abbreviation.toUpperCase() === request.params.id.toUpperCase()
+      })
+  */
 
-    const allTeams = teams.map((team) => {
-
-        return team
-
+    const matchingTeams = await models.Teams.findAll({
+        where: {
+            [Op.or]: [{ id: request.params.id }, { abbreviation: request.params.id }]
+        }
     })
-}) */
-
-
-
-app.get('/teams/:id', (request, response) => {
-
-
-    const matchingTeams = teams.filter((team) => {
-        return team.id === parseInt(request.params.id) || team.abbreviation.toUpperCase() === request.params.id.toUpperCase()
-    })
-
     // like and if else
     return matchingTeams.length
         ? response.send(matchingTeams)
@@ -42,19 +35,17 @@ app.get('/teams/:id', (request, response) => {
 
 app.use(bodyParser.json())
 
-app.post('/teams', (request, response) => {
+app.post('/teams', async (request, response) => {
 
-    //destruct
-    const { id, location, mascot, abbreviation, conference, division } = request.body
+    //deconstruct
+    const { location, mascot, abbreviation, conference, division } = request.body
 
     //check for values
-    if (!id || !location || !mascot || !abbreviation || !conference || !division) {
+    if (!location || !mascot || !abbreviation || !conference || !division) {
         response.status(400).send('didnt provide one of the following : id, location, mascot, abbreviation, conference, division')
     }
 
-    const newTeam = { id, location, mascot, abbreviation, conference, division }
-
-    teams.push(newTeam)
+    const newTeam = await models.Teams.create({ location, mascot, abbreviation, conference, division })
 
     // sends a created status
     response.status(201).send(newTeam)
@@ -64,94 +55,6 @@ app.post('/teams', (request, response) => {
 
 
 
-
-
-//check for nan
-/*if (isNaN(parseInt(request.params.id))) {
-
-    app.get('/teams/:abbreviation', (request, response) => {
-        console.log(typeof request.params.abbreviation)
-        const matchingAbbr = teams.filter((team) => {
-
-            return team.abbreviation === request.params.abbreviation
-
-        })
-
-        if (matchingAbbr.length) {
-
-            response.send(matchingAbbr)
-
-        } else {
-
-            response.status(404).send('Sorry, that Abbreviation doesnt exist')
-
-        }
-
-    })
-} else {
-    app.get('/teams/:id', (request, response) => {
-
-        const matchingID = teams.filter((team) => {
-
-            return team.id === parseInt(request.params.id)
-
-        })
-
-        if (matchingID.length) {
-
-            response.send(matchingID)
-
-        } else {
-
-            response.status(404).send('Sorry, that id doesnt exist')
-
-        }
-
-    })
-}
- */
-
-/*app.get('/teams/:id', (request, response) => {
-
-    const matchingID = teams.filter((team) => {
-
-        return team.id === parseInt(request.params.id)
-
-    })
-
-    if (matchingID.length) {
-
-        response.send(matchingID)
-
-    } else {
-
-        response.status(404).send('Sorry, that id doesnt exist')
-
-    }
-
-})
-
-
-
-app.get('/teams/:abbreviation', (request, response) => {
-    console.log(typeof request.params.abbreviation)
-    const matchingAbbr = teams.filter((team) => {
-
-        return team.abbreviation === request.params.abbreviation
-
-    })
-
-    if (matchingAbbr.length) {
-
-        response.send(matchingAbbr)
-
-    } else {
-
-        response.status(404).send('Sorry, that Abbreviation doesnt exist')
-
-    }
-
-}) */
 
 
 const server = app.listen(1337, () => { console.log('Listening on port 1337') })
